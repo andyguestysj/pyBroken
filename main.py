85de1aaaf9787a9020bf9656cbad5e9f5692bf3d
andyguest <a.guest@yorksj.ac.uk>
2020-09-28 08:58:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+print("I'm full of errors");
+
+##print(some errors will stop the code running)
+print("some will run but not give the right result")
+
+# print("hi mum")
+
+a_number = 3
+_another = 65
+#1_number = 11
+
+#This is a comment line
+
+pet1, pet2 = "cat", "dog"
+
+#pet3 = pet4 = pet5 = "dog" = "cat"
+
+a_string = "Hello World!"
+print(a_string[-1:-6])
+
+num_string = "10"
+num_int = int(num_string,2)
+num_int+=1
+print(str(num_int) + " is 11")
+
